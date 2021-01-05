29db7bff64a934562ee9d0cfc1560e6137d8d830
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2021-01-05 12:29:33 +0700

    a
    a

---
+++ js/style.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+function changeCirleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
